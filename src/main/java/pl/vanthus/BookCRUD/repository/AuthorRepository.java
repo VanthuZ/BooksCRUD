0d0f1812d634bde5ca122550bd348c4915175643
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-08-11 17:08:10 +0200

    Added author and book repositories

---
+++ src/main/java/pl/vanthus/BookCRUD/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package pl.vanthus.BookCRUD.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository {
+}
---
+++ src/main/java/pl/vanthus/BookCRUD/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package pl.vanthus.BookCRUD.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.vanthus.BookCRUD.model.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
