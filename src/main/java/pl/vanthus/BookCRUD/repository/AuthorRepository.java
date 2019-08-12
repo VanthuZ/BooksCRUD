0f5d9f8bfc8f3b0bc51b67021a8aad81017619c0
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-08-12 19:09:08 +0200

    Fixed extension

--- src/main/java/pl/vanthus/BookCRUD/repository/AuthorRepository.java
+++ src/main/java/pl/vanthus/BookCRUD/repository/AuthorRepository.java
@@ -2,7 +2,8 @@ package pl.vanthus.BookCRUD.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.vanthus.BookCRUD.model.Author;
 
 @Repository
-public interface AuthorRepository extends JpaRepository {
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
